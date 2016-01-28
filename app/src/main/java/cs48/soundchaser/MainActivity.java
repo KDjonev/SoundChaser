eff164632a3122f354ef53e9638f9272422fae55
Tricia Tsujimoto <tktsujimoto@gmail.com>
2016-01-27 21:58:40 -0800

    navigation menu added so buttons don't cover background

--- app/src/main/java/cs48/soundchaser/MainActivity.java
+++ app/src/main/java/cs48/soundchaser/MainActivity.java
@@ -3,6 +3,9 @@ package cs48.soundchaser;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Intent i;
+        switch(id) {
+            case R.id.settings :
+                i = new Intent(this, Settings.class);
+                startActivity(i);
+                break;
+            case R.id.profile :
+                i = new Intent(this, ProfileSettings.class);
+                startActivity(i);
+                break;
+            case R.id.viewData :
+                i = new Intent(this, ViewData.class);
+                startActivity(i);
+                break;
+            case R.id.help :
+                i = new Intent(this, Help.class);
+                startActivity(i);
+                break;
+        }
+        return true;
+    }
+
 
     public void onClick(View v)
     {
@@ -22,27 +65,6 @@ public class MainActivity extends AppCompatActivity {
             i = new Intent(this, OurGoogleMap.class);
             startActivity(i);
             break;
-
-            case R.id.helpButton :
-                i = new Intent(this, Help.class);
-                startActivity(i);
-                break;
-
-            case R.id.settingsButton :
-                i = new Intent(this, Settings.class);
-                startActivity(i);
-                break;
-
-            case R.id.profileButton:
-                i = new Intent(this, ProfileSettings.class);
-                startActivity(i);
-                break;
-
-            case R.id.viewDataButton :
-                i = new Intent(this, ViewData.class);
-                startActivity(i);
-                break;
-
         }
     }
 }
