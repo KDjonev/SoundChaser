30b1344c6835e6ca1b4eda6d113aef881badb1ab
Krassimir Djonev <kdjonev@umail.ucsb.edu>
2016-02-01 10:09:57 -0800

    google maps shows and zooms to our location

--- app/src/main/java/cs48/soundchaser/OurGoogleMap.java
+++ app/src/main/java/cs48/soundchaser/OurGoogleMap.java
@@ -1,80 +1,96 @@
 package cs48.soundchaser;
-
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.MapFragment;
 
-public class OurGoogleMap extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        GoogleMap.OnInfoWindowClickListener,
-        GoogleMap.OnMapLongClickListener,
-        GoogleMap.OnMapClickListener,
-        GoogleMap.OnMarkerClickListener {
+public class OurGoogleMap extends FragmentActivity implements LocationProvider.LocationCallback {
 
-    private com.google.android.gms.maps.GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mCurrentLocation;
+    public static final String TAG = OurGoogleMap.class.getSimpleName();
 
-    private final int[] MAP_TYPES = { GoogleMap.MAP_TYPE_SATELLITE,
-            GoogleMap.MAP_TYPE_NORMAL,
-            GoogleMap.MAP_TYPE_HYBRID,
-            GoogleMap.MAP_TYPE_TERRAIN,
-            GoogleMap.MAP_TYPE_NONE };
-    private int curMapTypeIndex = 0;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private LocationProvider mLocationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-    }
+        setUpMapIfNeeded();
 
-
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
+        mLocationProvider = new LocationProvider(this, this);
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
-
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mLocationProvider.connect();
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
     }
 
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
     }
 
-    @Override
-    public void onMapClick(LatLng latLng) {
-
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-    @Override
-    public void onMapLongClick(LatLng latLng) {
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
 
-    }
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
 
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
+        mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        float zoomLevel = 16; //This goes up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
     }
 }
